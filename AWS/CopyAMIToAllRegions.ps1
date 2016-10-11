cc9a303de40266e133b87abb65b5f88f186c470c
branislav-knezevic-devtech <branislav.knezevic@devtechgroup.com>
2016-10-11 16:37:35 +0200

    adding some aws scripts

---
+++ AWS/CopyAMIToAllRegions.ps1
@@ -0,0 +1,9 @@
+﻿$Regions = Get-EC2Region
+$ImageID =  ( Get-EC2Image | where { $_.OwnerId -like "989786818629" } ).ImageId
+ForEach ( $Region in $Regions ) {
+    $RegionName = $Region.RegionName
+    Set-DefaultAWSRegion -Region $RegionName
+    if ( (Get-EC2Image).ImageId -notcontains $imageID ) {
+        Copy-EC2Image -SourceRegion eu-central-1 -SourceImageId $ImageID -Region $RegionName
+    }
+}
---
+++ AWS/ListAllInstancesInAllRegions.ps1
@@ -0,0 +1,7 @@
+﻿$Regions = Get-EC2Region
+ForEach ( $Region in $Regions ) {
+    $RegionName = $Region.RegionName
+    Set-DefaultAWSRegion -Region $RegionName
+    Write-Output `n "Instances in $RegionName :"
+    (Get-EC2Instance).Instances | select ImageId,InstanceType,KeyName,LaunchTime 
+}
