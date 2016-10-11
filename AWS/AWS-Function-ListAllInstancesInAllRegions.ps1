630ea200d33d0da81556c9eff1fc2c9e41f1f18c
branislav-knezevic-devtech <branislav.knezevic@devtechgroup.com>
2016-10-11 16:55:02 +0200

    renaming couple of files and making function version of one command

---
+++ AWS/AWS-Function-ListAllInstancesInAllRegions.ps1
@@ -0,0 +1,13 @@
+﻿# funkctioon Get-AllInstances lists all running instances grouped by region
+
+function Get-AllInstances {
+
+    $Regions = Get-EC2Region
+    $AllInstances = ForEach ( $Region in $Regions ) {
+        $RegionName = $Region.RegionName
+        Set-DefaultAWSRegion -Region $RegionName
+        Write-Output `n "Instances in $RegionName :"
+        (Get-EC2Instance).Instances | select ImageId,InstanceType,KeyName,LaunchTime 
+    }
+    Return $AllInstances
+}
