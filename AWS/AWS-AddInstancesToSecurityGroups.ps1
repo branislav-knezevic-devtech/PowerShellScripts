4f5a9ee1b0f23d70377dc827dca9ef3366c18702
branislav-knezevic-devtech <branislav.knezevic@devtechgroup.com>
2016-10-11 16:40:59 +0200

    Some more AWS scripts

---
+++ AWS/AWS-AddInstancesToSecurityGroups.ps1
@@ -0,0 +1,4 @@
+﻿$groupname = (Get-EC2SecurityGroup -GroupName 'group-name','group-name').GroupId
+$instanceid = (Get-EC2Instance -InstanceId 'instance id').instances.instanceid
+Edit-EC2InstanceAttribute -InstanceId $instanceid -Group $groupname
+#all groups must be listed or the command will overrite the existing ones
