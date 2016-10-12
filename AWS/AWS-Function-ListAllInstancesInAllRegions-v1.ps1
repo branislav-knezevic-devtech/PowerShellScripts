77fd189dc739ebeb8ce8aaca0cd0c663b0508d4b
branislav-knezevic-devtech <branislav.knezevic@devtechgroup.com>
2016-10-12 16:48:49 +0200

    saving before adding the select line instead of the array

--- AWS/AWS-Function-ListAllInstancesInAllRegions-v1.ps1
+++ AWS/AWS-Function-ListAllInstancesInAllRegions-v1.ps1
@@ -7,36 +7,46 @@ ForEach ( $Region in $Regions )
     {
         $RegionName = $Region.RegionName
         Set-DefaultAWSRegion -Region $RegionName -ErrorAction Stop
-        $Instances = (Get-EC2Instance).Instances # | select ImageId,InstanceType,KeyName,LaunchTime
-        if ( $instances.count -gt 0 )
+        $Instances = (Get-EC2Instance).Instances
+        $InstancesCount = $Instances.count 
+        if ($InstancesCount -gt 0)
         { 
-            $InstanceProperties = @{ RegionName = $RegionName
-                                     RegionStatus = 'Available'
-                                     InstanceID = $Instances.imageid
-                                     InstanceType = $Instances.InstanceType
-                                     KeyName = $Instances.KeyName
-                                     LaunchTime = $Instances.LaunchTime } 
+            $InstanceProperties = @{RegionName = $RegionName
+                                    RegionStatus = 'Available'
+                                    InstanceID = $Instances.imageid
+                                    InstanceType = $Instances.InstanceType
+                                    KeyName = $Instances.KeyName
+                                    LaunchTime = $Instances.LaunchTime} 
         }
     }
     catch
     {
-         $InstanceProperties = @{ RegionName = $RegionName
-                                     RegionStatus = 'Unvailable'
-                                     InstanceID = $null
-                                     InstanceType = $null
-                                     KeyName = $null
-                                     LaunchTime = $null } 
+        Write-Warning "AWS is having problems to connect to region $RegionName"
+        $InstanceProperties = @{RegionName = $RegionName
+                                RegionStatus = 'Unvailable'
+                                InstanceID = $null
+                                InstanceType = $null
+                                KeyName = $null
+                                LaunchTime = $null} 
     }
     finally
     {
-       if ( $instances.count -gt 0 )
-       {
+        if ($instances.count -gt 0)
+        {
             $obj = New-Object -TypeName PSObject -Property $InstanceProperties
+            if ($InstancesCount -eq 1)
+            {
+                Write-Output "$InstancesCount instance found in $RegionName"
+            }
+            else
+            {
+                Write-Output "$InstancesCount instances found in $RegionName"
+            }
             Write-Output $obj
-       }
-       Else
-       {
-            Write-Host "There are no instances in region $RegionName"
-       }
+        }
+        Else
+        {
+            Write-Output "There are no instances in region $RegionName"
+        }
     }
 }
