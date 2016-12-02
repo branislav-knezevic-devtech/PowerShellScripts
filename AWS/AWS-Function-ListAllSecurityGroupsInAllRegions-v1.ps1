f8201e3e4c352bb7fce30407fb231200aba11505
branislav-knezevic-devtech <branislav.knezevic@devtechgroup.com>
2016-12-02 17:00:28 +0100

    O365 users converted from script to function

---
+++ AWS/AWS-Function-ListAllSecurityGroupsInAllRegions-v1.ps1
@@ -0,0 +1,47 @@
+﻿# Get all running instances in all regions following Don Jones tool making part guide
+
+$Regions = Get-EC2Region
+ForEach ( $Region in $Regions ) 
+{
+    try 
+    {
+        $RegionName = $Region.RegionName
+        Set-DefaultAWSRegion -Region $RegionName -ErrorAction Stop
+        $Group = (Get-EC2SecurityGroup | where { $_.Description -like "launch-wizard*" })
+        $GroupCount = $Groups.count 
+        if ($GroupCount -gt 0)
+        { 
+            $GroupProperties = $Group | select description,groupID,groupName | fl
+        }
+    }
+    catch
+    {
+        Write-Warning "AWS is having problems to connect to region $RegionName"
+    }
+    finally
+    {
+        if ($Group.count -gt 0)
+        {
+            if ($GroupCount -eq 1)
+            {
+                Write-Output "$GroupCount Group found in $RegionName"
+            }
+            else
+            {
+                Write-Output "$GroupCount Groups found in $RegionName"
+            }
+            Write-Output $GroupProperties
+        }
+        Else
+        {
+            Write-Output "There are no Groups in region $RegionName"
+        }
+    }
+}
+
+<#
+can be added here
+BEGIN {}
+PROCESS {}
+END {}
+#>
--- Create multiple O365 Users.ps1
+++ Create multiple O365 Users.ps1
@@ -1,72 +1,82 @@
-﻿$domain = "@devcmp20.onmicrosoft.com"
-$UserCredential = Get-Credential
+﻿
+function Create-NewO365Destination
+{
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory=$true,
+                   Position=1,
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$False)]
+        [String]$domain 
+    )
+    if ($domain -like "devcmp*.onmicrosoft.com")
+    {
 
-Connect-MsolService
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+        # connect to O365
+        $UserCredential = Get-Credential
+        
+        Connect-MsolService
+        $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+        
+        Import-PSSession $Session
+        
+        # create an array of users which will be created
+        $users = New-Object System.Collections.ArrayList
+        $users.Add("atila bala")
+        $users.Add("nemanja tomic")
+        $users.Add("fedor hajdu")
+        $users.Add("milan stojanovic")
+        $users.Add("slavisa radicevic")
+        $users.Add("paula novokmet")
+        $users.Add("robert sebescen")
+        $users.Add("dragan eremic")
+        $users.Add("vladimir pecanac")
+        $users.Add("milivoj kovacevic")
+        $users.Add("martin jonas")
+        $users.Add("dragana berber")
+        $users.Add("danijel avramov")
+        $users.Add("dejan babic")
+        $users.Add("Babara Harcharik")
+        $users.Add("Brenton Byus")
+        $users.Add("Catrice Hartz")
+        $users.Add("Doris Luening")
+        $users.Add("Ebony Tott")
+        $users.Add("Florentino Snobeck")
+        $users.Add("Ila Lockamy")
+        $users.Add("Lovie Geronime")
+        $users.Add("Lucretia Sangalli")
+        $users.Add("Randell Fleniken")
+        
+        # Crate user account from users in the array
+        foreach ($user in $users) 
+        {
+            $u = New-Object System.Collections.ArrayList
+            $u = $user.Split(" ")
+            
+            $first = $u[0]
+            $last = $u[1]
+            
+            $upn = $first + "." + $last + $domain
+            
+            New-MsolUser -FirstName $first -LastName $last -UserPrincipalName $upn -Password m1cr0s0ft$ -DisplayName $user -PasswordNeverExpires $true -ForceChangePassword $false
+            
+            set-msoluser -userprincipalname $upn -usagelocation RS
+            
+            $tenant = (Get-MsolAccountSku).AccountObjectId
+            Set-MsolUserLicense -TenantId $tenant -UserPrincipalName $upn -AddLicenses (Get-MsolAccountSku -TenantId $tenant).AccountSkuId
+        }
+    }
+    else
+    {
+        $ErrorText = "Domain must be in devcmpXX.onmicrosoft.com format.
+        Youre entry is: $domain"
+        Write-Host $ErrorText -ForegroundColor Red
+    }
 
-Import-PSSession $Session
-
-
-$users = New-Object System.Collections.ArrayList
-#$users.Add("goran manot")
-$users.Add("atila bala")
-$users.Add("nemanja tomic")
-$users.Add("fedor hajdu")
-$users.Add("milan stojanovic")
-$users.Add("slavisa radicevic")
-$users.Add("paula novokmet")
-$users.Add("robert sebescen")
-$users.Add("dragan eremic")
-$users.Add("vladimir pecanac")
-$users.Add("milivoj kovacevic")
-$users.Add("martin jonas")
-$users.Add("dragana berber")
-$users.Add("danijel avramov")
-$users.Add("dejan babic")
-#$users.Add("vladimir kreko")
-#$users.Add("milos tomasevic")
-#$users.Add("filip uzunovic")
-#$users.Add("milos mokic")
-#$users.Add("snezana ralic")
-#$users.Add("marko pap")
-#$users.Add("mile misan")
-#$users.Add("velibor glisin")
-#$users.Add("vladislav herbut")
-#$users.Add("ivica kolenkas")
-$users.Add("Babara Harcharik")
-$users.Add("Brenton Byus")
-$users.Add("Catrice Hartz")
-$users.Add("Doris Luening")
-$users.Add("Ebony Tott")
-$users.Add("Florentino Snobeck")
-$users.Add("Ila Lockamy")
-$users.Add("Lovie Geronime")
-$users.Add("Lucretia Sangalli")
-$users.Add("Randell Fleniken")
-
-foreach ($user in $users) {
-$u = New-Object System.Collections.ArrayList
-$u = $user.Split(" ")
-
-
-$first = $u[0]
-$last = $u[1]
-
-$upn = $first + "." + $last + $domain
-
-New-MsolUser -FirstName $first -LastName $last -UserPrincipalName $upn -Password m1cr0s0ft$ -DisplayName $user -PasswordNeverExpires $true -ForceChangePassword $false
-
-set-msoluser -userprincipalname $upn -usagelocation RS
-
-$tenant = (Get-MsolAccountSku).AccountObjectId
-Set-MsolUserLicense -TenantId $tenant -UserPrincipalName $upn -AddLicenses (Get-MsolAccountSku -TenantId $tenant).AccountSkuId
-
-
+    # apply impersonation rights for goran.manot user on whole domain
+    Enable-OrganizationCustomization
+    $User = "goran.manot" + $domain
+    New-ManagementRoleAssignment -Role ApplicationImpersonation -User $User
+    Remove-PSSession $Session
 }
-
-Enable-OrganizationCustomization
-$User = "goran.manot" + $domain
-New-ManagementRoleAssignment -Role ApplicationImpersonation -User $User
-#Disable-Organi
-#>
-Remove-PSSession $Session
