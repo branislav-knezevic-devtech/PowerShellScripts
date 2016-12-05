b9672f622621fb717096e8d57d60dafad117397e
branislav-knezevic-devtech <branislav.knezevic@devtechgroup.com>
2016-12-05 14:31:45 +0100

    Converting connect file to functon

--- Connect.ps1
+++ Connect.ps1
@@ -1,3 +1,42 @@
-$LiveCred = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
-Import-PSSession $Session
+function Connect-O365_BK
+{
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory=$true,
+                   Position=1,
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$False)]
+        [String]$domain 
+    )
+
+    $fullDomain = $domain + ".onmicrosoft.com"
+    $AdminName = "goran.manot" + "@" + $fullDomain 
+    $Pass = Get-Content "D:\Credentials\Credentials.txt" | ConvertTo-SecureString
+    $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminName, $Pass
+    
+    if ($fullDomain -like "devcmp*.onmicrosoft.com")
+    {
+        try
+        {
+            $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
+            Import-PSSession $Session
+        }
+        catch
+        {
+            Write-Output "Connection has failed"
+            Write-Output $_.ErrorID
+            Write-Output $_.Exception.Message
+            break
+        }
+    }
+    else
+    {
+        $ErrorText = "Domain must be in devcmpXX
+        Your entry is: $fullDomain"
+        Write-Host $ErrorText -ForegroundColor Red
+        break
+    }
+}
+
+ 
